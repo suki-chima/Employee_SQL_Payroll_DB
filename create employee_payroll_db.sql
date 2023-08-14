d6cabec653b847c762b41d80540087082a72ecba
suki chima <34716849+suki-chima@users.noreply.github.com>
2023-08-14 12:59:00 +0100

    Add files via upload

---
+++ create employee_payroll_db.sql
@@ -0,0 +1,10 @@
+/*
+DROP DATABASE IF EXISTS employee_payroll_2023;
+
+CREATE DATABASE employee_payroll_2023;
+
+USE employee_payroll_2023;
+
+DROP TABLE IF EXISTS employee_info, department, jobdetails, payinfo ;
+
+*/
